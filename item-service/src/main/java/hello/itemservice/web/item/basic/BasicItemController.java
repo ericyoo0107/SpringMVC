d0fd58141364222202d49bf95639879b0dd91749
YuJunHyuk <ericyoo0107@gmail.com>
2022-12-22 01:31:41 +0900

    feat: 상품 수정하는 기능 추가

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -36,7 +36,7 @@ public class BasicItemController {
 		return "basic/addForm";
 	}
 
-//	@PostMapping("/add")
+	//	@PostMapping("/add")
 	public String addItemV1(@RequestParam String itemName,
 		@RequestParam int price,
 		@RequestParam Integer quantity, Model model) {
@@ -49,7 +49,7 @@ public class BasicItemController {
 		return "basic/item";
 	}
 
-//	@PostMapping("/add")
+	//	@PostMapping("/add")
 	public String addItemV2(@ModelAttribute("item") Item item, Model model) {
 		itemRepository.save(item);
 		model.addAttribute("item", item);
@@ -60,6 +60,19 @@ public class BasicItemController {
 	public String addItemV3(@ModelAttribute Item item) {
 		itemRepository.save(item);
 		return "basic/item";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+		itemRepository.update(itemId, item);
+		return "redirect:/basic/items/{itemId}";
 	}
 
 	@PostConstruct
